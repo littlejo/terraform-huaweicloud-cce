79990aeb302ca5135c60a4b57da6e1960637ef33
Joseph Ligier <joseph.ligier@accenture.com>
2021-12-23 12:43:31 +0100

    fmt

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "name" {
 
 variable "flavor_id" {
   description = "Flavor of the cce cluster"
-  default = "cce.s2.small"
+  default     = "cce.s2.small"
 }
 
 variable "vpc_id" {
@@ -17,12 +17,12 @@ variable "subnet_id" {
 
 variable "node_pool_name" {
   description = "Name of the cce node pool"
-  default = "generic"
+  default     = "generic"
 }
 
 variable "node_pool_flavor_id" {
   description = "Flavor of the cce node pool"
-  default = "s3.large.4"
+  default     = "s3.large.4"
 }
 
 variable "node_pool_key_pair" {
@@ -31,24 +31,24 @@ variable "node_pool_key_pair" {
 
 variable "addon_ingress_loadbalancer_ip" {
   description = "Load balancer ip for nginx-ingress addon"
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "addon_ingress_config" {
   description = "nginx config for nginx-ingress addon"
-  type    = map(any)
-  default = {}
+  type        = map(any)
+  default     = {}
 }
 
 variable "addon_ingress_resource" {
   description = "nginx resource for nginx-ingress addon"
-  type    = map(any)
+  type        = map(any)
   default = {
-              limitsCpu   = "1024m"
-              limitsMem   = "1024Mi"
-              name        = "nginx-ingress"
-              requestsCpu = "256m"
-              requestsMem = "256Mi"
-            }
+    limitsCpu   = "1024m"
+    limitsMem   = "1024Mi"
+    name        = "nginx-ingress"
+    requestsCpu = "256m"
+    requestsMem = "256Mi"
+  }
 }
